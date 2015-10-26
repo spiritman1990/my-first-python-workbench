acf0a68a91ee73dba1be9663cd8bd62708cca356
spiritman1990 <36917663@qq.com>
2015-10-26 17:45:09 +0800

    2015年10月26日17:44:46 转换二进制正整数为二进制 2015年10月26日17:55:37 检查git hub上未成功上传更新

---
+++ bincalc.py
@@ -0,0 +1,30 @@
+#!/bin/python
+# coding: gbk
+import time
+num = raw_input("请输入一个整数:")
+while not num.isdigit():
+    num = raw_input("请重新输入一个正整数:")
+else:
+    num = int(num)
+print "输入的数字是",num
+print "开始求",num,"的二进制表示方法:"
+s='.'
+for c in range(1,10):
+    d = c * 10
+    time.sleep(0.5)
+    print "计算进度：%d%% [ " % d + s*c +" ]","\n"
+res = ""
+ 
+while (num-1)/2 >=0:
+    if num%2:
+        res = res+"1"
+        num = (num-1)/2 
+    elif not num%2:
+        res = res+"0"
+        num = num/2        
+if len(res) < 8:
+    res = '0'*(8-len(res))+res
+else:
+    res = res
+print "结果是:",res    
+    
